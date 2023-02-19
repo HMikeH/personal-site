e4082163224b29642c988c864639bf09f9b82556
HMikeH <yoyohsy@yahoo.com.tw>
2023-02-19 22:45:44 +0800

    modify js error

--- js/resume.js
+++ js/resume.js
@@ -10,20 +10,41 @@ let All_rd = {"wk_aiec":["rd-ner","rd-dm","rd-db","rd-tm","rd-vis"],
               "wk_ssic":["rd-ner","rd-dm","rd-db","rd-tm","rd-vis","rd-topic","rd-hlda","rd-im"],              
               "wk_dlll":["rd-dm"]}
 
+let All_wk = ["wk_aiec","wk_ssic","wk_dlll"]
 
 
-
+// TO DO:直接不包括的項目變灰即可
 function highlight(elem){
-    let _sk_items = document.querySelector(".sk-items");
-    _sk_items.style.color = "#DEDEDE";
-    let _rd_items = document.querySelector(".rd-items");
-    _rd_items.style.color = "#DEDEDE";
 
-    // 尚未做其他wk的反灰
-    // elem.style.color = "black";
+    // 第0步:主項目之外便灰
+    elem.style.color = "black";
     let _name=$(elem).attr("class");
     console.log(_name);
 
+    for(i=0;i<All_wk.length;i++){
+        console.log(All_wk[i])
+        if (All_wk[i] !=_name){
+            console.log(All_wk[i])
+            let aa = document.getElementById(All_wk[i]); 
+            aa.style.color ="#DEDEDE";
+        }
+    }
+
+    
+    // 第1步:全部變灰
+    for(i=0;i<sk_list.length;i++){
+        let ff = document.getElementById(sk_list[i]);
+        ff.style.color = "#DEDEDE";
+    }
+    for(i=0;i<rd_list.length;i++){
+        let gg = document.getElementById(rd_list[i]);
+        gg.style.color = "#DEDEDE";
+
+    }
+    
+    
+
+    // 第2步:有的項目變黑
     for(i=0;i<All_sk[_name].length;i++){
         // console.log(All_sk[_name][i]);
         let cc = document.getElementById(All_sk[_name][i]);
@@ -31,7 +52,6 @@ function highlight(elem){
     }
 
     for(i=0;i<All_rd[_name].length;i++){
-        // console.log(All_sk[_name][i]);
         let dd = document.getElementById(All_rd[_name][i]);
         dd.style.color = "black";
     }
@@ -39,12 +59,21 @@ function highlight(elem){
     
 }
 
+
 function out(elem){
-    let _sk_items = document.querySelector(".sk-items");
-    _sk_items.style.color = "black";
-    let _rd_items = document.querySelector(".rd-items");
-    _rd_items.style.color = "black";
-    elem.style.color = "black";
+    for(i=0;i<All_wk.length;i++){
+        let aa = document.getElementById(All_wk[i]);
+        aa.style.color = "black";
+    }
+    for(i=0;i<sk_list.length;i++){
+        let ff = document.getElementById(sk_list[i]);
+        ff.style.color = "black";
+    }
+    for(i=0;i<rd_list.length;i++){
+        let gg = document.getElementById(rd_list[i]);
+        gg.style.color = "black";
+    }
+
 }
 
 
